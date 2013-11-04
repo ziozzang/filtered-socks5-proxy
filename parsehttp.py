a9c4d27726aa8803a1c663ae0e450277c4247d68
Jioh L. Jung <ziozzang@gmail.com>
2013-11-04 09:44:58 +0900

    Update parsehttp.py

--- parsehttp.py
+++ parsehttp.py
@@ -1,4 +1,9 @@
 # -*- coding:utf-8 -*-
+#
+# Utility Functions of HTTP packet filtering feture.
+# - Code by Jioh L. Jung.(ziozzang@gmail.com)
+#
+
 import re
 
 #HTTP 1.1 spec said that spliter is \r\n
@@ -11,6 +16,7 @@ def fixcontentlength(body, sp="\r\n"):
   hdr = re.sub(r"Content-Length[^\r\n]*", "Content-Length: %d" % (cntlen), hdr)
   return hdr + "\r\n\r\n" + cnt
 
+# split content into header and body parts
 def splitbody(body, sp="\r\n"):
   s = len(sp)
   pos = body.find(sp*2)
@@ -18,7 +24,8 @@ def splitbody(body, sp="\r\n"):
   cnt = body[pos+s*2:]
   return (hdr, cnt)
 
-def getheaders(body):
+# Get Header lines list.
+def getheaderslist(body):
   headers = []
   for i in body.splitlines():
     if len(i) == 0:
@@ -26,7 +33,16 @@ def getheaders(body):
     else:
       headers.append(i)
   return headers
+  
+# Get only Header Parts from contents.
+def getheaderpartonly(body, sp="\r\n"):
+  s = len(sp)
+  pos = body.find(sp*2)
+  hdr = body[:pos]
+  return hdr
 
+# Sample code for fixing stream data.
+# - for match Content length, Resize of 'Content-Length' Header value.
 def fixcontent_sample(body):
   hdr, bdy = splitbody(body)
   bdy = re.sub(r"<TagName>[^<]*</TagName>", "<TagName>0</TagName>", bdy)
